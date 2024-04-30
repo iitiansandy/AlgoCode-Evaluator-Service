8883d01e3d8ea3776a21e42d70fb5fc142c5216d
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-04-30 23:38:56 +0530

    files modified

--- src/controllers/pingController.ts
+++ src/controllers/pingController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export const pingCheck = (req: Request, res: Response) => {
+export const pingCheck = (_: Request, res: Response) => {
     return res.status(200).json({
         message: "Ping Check Ok"
     });
