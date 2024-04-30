26409b49ed1d3116559007d1ead26fb3253f12dd
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-04-30 23:37:49 +0530

    Feat: Basic API setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig";
+import apiRouter from "./routes";
 
-const app = express();
+const app: Express = express();
+
+app.use('/api', apiRouter);
 
 app.listen(serverConfig.port, () => {
     console.log("Server is running on port", serverConfig.port);
