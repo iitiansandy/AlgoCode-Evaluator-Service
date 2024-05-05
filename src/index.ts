e61a6ad9a699c8e3c1efbe0c4be1e1a2205313e8
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-05-05 22:33:37 +0530

    BullMq based redis queue infra up and runing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
+import SampleWorker from "./workers/sampleWorker";
 
 const app: Express = express();
 
@@ -9,4 +11,13 @@ app.use('/api', apiRouter);
 
 app.listen(serverConfig.port, () => {
     console.log("Server is running on port", serverConfig.port);
+
+    SampleWorker('SampleQueue');
+
+    sampleQueueProducer('SampleJob', {
+        name: "Sandeep",
+        company: 'IBM',
+        position: 'SDE-1',
+        location: "Noida"
+    });
 });
