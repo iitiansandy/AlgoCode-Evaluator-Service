d6c950c773e110a1747257d5ac479c67eee12290
iitiansandy <sandeep.sid.kumar10@gmail.com>
2024-06-22 14:19:33 +0530

    added code creator

---
+++ src/utils/codeCreator.ts
@@ -0,0 +1,16 @@
+export default function pythonCodeCreator(startingCode: string, middleCode: string, endCode: string): string {
+    return `
+        ${startingCode}
+
+        ${middleCode}
+
+        ${endCode}
+    `;
+}
+
+/**
+ * for python, end code can be passed as empty string
+ *
+ * for java, end code can be passed as empty string
+ *
+ */
